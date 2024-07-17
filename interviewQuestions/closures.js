181ed334ea4290111b866df6e322081899dcd8f2
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-07-17 15:00:10 +0530

    closures and lexical scoping

---
+++ interviewQuestions/closures.js
@@ -0,0 +1,24 @@
+// When you create a closure, the inner function "remembers" the variables from its outer functions because of lexical scoping.
+
+//Closure: An inner function remembers variables from its outer functions because of lexical scoping, even after the outer function has finished running.
+
+//Lexical Scoping: Like looking for toys in rooms, JavaScript looks for variables in nested functions based on where the code is written.
+
+//when we return inner function, it gets returned with its lexical environement(like variables and all)...it not just return function, it return lexical scope
+
+//closure = function along with scope (simple defination)
+
+//==============================================
+    function makeFunc() {
+        var name = "Mozilla";
+        function displayName() {
+            console.log(name);
+        }
+        name = "Akshay"
+        return displayName;   //return karne se jo bhi uss func se jude hue h vo bhi return ho jate h pura scope return hota h
+    }
+
+    const myFunc = makeFunc();
+    console.log(myFunc);
+    myFunc();
+//==============================================
